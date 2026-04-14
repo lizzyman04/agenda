6ca78ac8282fdaba5a144086d648c86eafb91314
lizzyman04 <arlindoabdullm@gmail.com>
2026-04-14 06:31:01 +0200

    test(02-01): add domain test stubs for item, eisenhower_quadrant, recurrence_engine
    - Create test/domain/tasks/item_test.dart stub
    - Create test/domain/tasks/eisenhower_quadrant_test.dart stub
    - Create test/domain/tasks/recurrence_engine_test.dart stub

---
+++ test/domain/tasks/eisenhower_quadrant_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('EisenhowerQuadrant', () {
+    // Tests added in Wave 1
+  });
+}
---
+++ test/domain/tasks/recurrence_engine_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('RecurrenceEngine', () {
+    // Tests added in Wave 1
+  });
+}
